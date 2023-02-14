676ae18ede8ddbc1a9821b3945e9b30c51d6a402
khareenp <khareenp1@gmail.com>
2023-02-14 12:59:47 -0500

    created first component

--- P1-Frontend/src/components/Header/Header.tsx
+++ P1-Frontend/src/components/Header/Header.tsx
@@ -1,4 +1,6 @@
+import "./Header.css";
+
 function Header() {
-  return <div>This is the page header</div>;
+  return <div className="red-text">This is the page header</div>;
 }
 export default Header;
